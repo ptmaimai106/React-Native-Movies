542aabbc8ce09abd0df688f31c11e64b4a67b85a
ptmaimai106 <ptmaimai106@gmail.com>
2019-11-12 17:55:38 +0700

    Search bar

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {AppNavigator} from './AppNavigator';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const AppContainer = createAppContainer(
   createBottomTabNavigator(
@@ -11,18 +10,6 @@ const AppContainer = createAppContainer(
       Settings: {screen: AppNavigator},
     },
     {
-      navigationOptions: ({navigation}) => ({
-        tabBarIcon: ({focused, tintColor}) => {
-          const {routeName} = navigation.state;
-          let iconName;
-          if (routeName === 'Home') {
-            iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-          } else if (routeName === 'Settings') {
-            iconName = `ios-options${focused ? '' : '-outline'}`;
-          }
-          return <Ionicons name={iconName} size={25} color={tintColor} />;
-        },
-      }),
       tabBarOptions: {
         activeTintColor: 'tomato',
         inactiveTintColor: 'gray',
